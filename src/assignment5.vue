5f3e8d7a237f4f81d974d9eb8276a0d41c47e0c0
Oduwole Hameed <hameedoduwole@gmail.com>
2022-10-11 02:28:22 +0100

    Create assignment5.vue
    Todo APP

---
+++ src/assignment5.vue
@@ -0,0 +1,156 @@
+<template>
+  <div>
+    <header>
+      <h1>Vue Lists and Conditional Content</h1>
+    </header>
+
+    <pre>
+     user_input = {{ user_input }}
+     list_of_tasks = {{ list_of_tasks }}
+     show_list = {{ show_list }}
+    </pre>
+
+    <section id="assignment">
+      <h2>Assignment</h2>
+      <!-- 1) Add code to manage a list of tasks in a Vue app -->
+      <!-- When clicking "Add Task" a new task with the entered text should be added -->
+      <input v-model="user_input" type="text" />
+      <button @click="addTask">Add Task</button>
+      <ul v-if="show_list">
+        <li
+          v-for="(each, index) in list_of_tasks"
+          :key="each"
+          @click="removeTask(index)"
+        >
+          {{ each }} - {{ index }}
+        </li>
+        <!-- 2) Output the list of tasks here -->
+      </ul>
+      <!-- 3) When the below button is pressed, the list should be shown or hidden -->
+      <!-- BONUS: Also update the button caption -->
+      <br />
+      <button @click="listToggle">{{ buttonCaption }}</button>
+    </section>
+  </div>
+</template>
+
+
+
+
+<script>
+export default {
+  data: () => {
+    return {
+      user_input: "",
+      list_of_tasks: [],
+      show_list: true,
+    };
+  },
+  computed: {
+    buttonCaption() {
+      return this.show_list ? "Show List" : "Hide List";
+    },
+  },
+  methods: {
+    addTask() {
+      this.list_of_tasks.push(this.user_input);
+      this.user_input = "";
+    },
+    removeTask(id) {
+      this.list_of_tasks.splice(id, 1);
+    },
+    listToggle() {
+      this.show_list = !this.show_list;
+    },
+  },
+};
+</script>
+
+
+
+
+
+
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #1b995e;
+  color: white;
+  text-align: center;
+}
+
+#assignment {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+}
+
+#assignment h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #1b995e;
+  margin: 0 0 1rem 0;
+}
+
+#assignment ul {
+  list-style: none;
+  margin: 1rem 0;
+  padding: 0;
+}
+
+#assignment li {
+  margin: 1rem 0;
+  font-size: 1.25rem;
+  font-weight: bold;
+  background-color: #8ddba4;
+  padding: 0.5rem;
+  color: #1f1f1f;
+  border-radius: 25px;
+}
+
+#assignment input {
+  font: inherit;
+  border: 1px solid #ccc;
+}
+
+#assignment input:focus {
+  outline: none;
+  border-color: #1b995e;
+  background-color: #d7fdeb;
+}
+
+#assignment button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#assignment button:hover,
+#assignment button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
